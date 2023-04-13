c30eaae63950b4831ee682109ace5912c096861f
Anthony Cole <anthony.cole83@yahoo.com>
2023-04-13 14:32:05 -0500

    Made a form for setting new description and new image for the POST route.

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
@@ -6,6 +6,9 @@ import { useDispatch } from "react-redux";
 function ShelfPage() {
   const dispatch = useDispatch();
   const shelfItems = useSelector((store) => store.shelfItems);
+  const [newItem, setNewItem] = useState('')
+  const [newDescription, setNewDescription] = useState('')
+  const [newImage, setNewImage] = useState('')
   console.log('shelf items:', shelfItems);
 
   useEffect(() => {
@@ -14,7 +17,43 @@ function ShelfPage() {
     });
   }, []);
 
+const addNewItem = (event)=>{
+event.preventDefault();
+dispatch({
+  type: 'ADD_ITEM',
+  payload: newItem
+})
+
+const changeDescription = (event)=>{
+    setNewDescription(event.target.value)
+
+}
+
+const changeImage = (event)=>{
+    setNewImage(event.target.value)
+}
+
+}
+
+
   return (
+<>
+    <form onSubmit={addNewItem}>
+      <input
+      type='text'
+      value={newDescription}
+      onChange={(event)=> changeDescription(event)}
+      placeholder='description'>
+      </input>
+      <input
+      type='text'
+      value={newImage}
+      onChange={(event)=> changeImage(event)}
+      placeholder='image'
+      ></input>
+    </form>
+
+
     <div className="container">
       <h2>Shelf</h2>
       <div>
@@ -29,6 +68,7 @@ function ShelfPage() {
           })}
       </div>
     </div>
+    </>
   );
 }
 
